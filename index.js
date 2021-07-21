a65ad54987351d5c0b83558875c4409352987b43
Edward <edward.arechwa@gogomedia.pl>
2021-07-21 14:42:29 +0200

    add hbs and default layout

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const expHbs = require("express-handlebars");
+const path = require("path");
 
 const app = express();
+
+const hbs = expHbs.create({
+    defaultLayout: "main",
+    extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.get("/", (_, response) => {
+    response.render("index");
+});
+
+app.get("/about", (_, response) => {
+    response.render("about");
+});
 
 const PORT = process.env.PORT || 3000;
 
