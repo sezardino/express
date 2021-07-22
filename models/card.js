9765a0ab00f4ae95be16af00c9874ab26376f813
Edward <edward.arechwa@gogomedia.pl>
2021-07-22 08:46:02 +0200

    card: add to, view

--- models/card.js
+++ models/card.js
@@ -1,11 +1,48 @@
+const path = require("path");
+const fs = require("fs");
+
+const cardFilePath = path.join(__dirname, "..", "data", "card.json");
+
 class Card {
-  static add(id) {
-    console.log(id);
-  }
+    static async add(course) {
+        const card = await Card.fetch();
 
-  static fetch() {
+        const courseIndex = card.courses.findIndex(
+            (item) => item.id === course.id
+        );
+        const courseInCard = card.courses[courseIndex];
 
-  }
+        if (courseInCard) {
+            courseInCard.count++;
+        } else {
+            course.count = 1;
+            card.courses.push(course);
+        }
+
+        card.price += +course.price;
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(cardFilePath, JSON.stringify(card), (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    static async fetch() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(cardFilePath, "utf-8", (error, content) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(JSON.parse(content));
+                }
+            });
+        });
+    }
 }
 
 module.exports = Card;
--- routes/card.js
+++ routes/card.js
@@ -1,17 +1,24 @@
 const { Router } = require("express");
+const Course = require("../models/course");
 const Card = require("../models/card");
 
 const router = Router();
 
-router.get("/", (request, response) => {
-  response.render("card", {
-    isCard: true,
-    title: 'Card'
-  });
+router.get("/", async (request, response) => {
+    const card = await Card.fetch();
+    response.render("card", {
+        isCard: true,
+        title: "Card",
+        courses: card.courses,
+        price: card.price,
+    });
 });
 
-router.post("/add", (request, response) => {
-  Card.add(request.body);
+router.post("/add", async (request, response) => {
+    const course = await Course.getById(request.body.id);
+    Card.add(course);
+
+    response.redirect("/card");
 });
 
 module.exports = router;
